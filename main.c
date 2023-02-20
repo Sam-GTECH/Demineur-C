5cf85eca608efba5472eb1c06cda09db045aba74
Sam-GTECH <sburton@gaming.tech>
2023-02-20 13:40:41 +0100

    Something something

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
+#include <time.h>
 #include <stdio.h>
 
 int main() {
+
+    int matrice[5][5] = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+    };
+
+    srand(time(0));
+    int r = (rand() % (1 - 0 + 1)) + 0;
+    printf("The random number is %d!\n\n", r);
+
+    int row, columns;
+    for (row=0; row<2; row++)
+    {
+        for(columns=0; columns<2; columns++)
+        {
+            printf("-%d-", matrice[row][columns]);
+        }
+        printf("\n");
+    }
+
+    printf("Hello world! The answer to life is %p", *matrice);
+
     return 0;
 }
+
