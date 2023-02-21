2f44723ef330978a79ce307ae30dc5fa58d8ed65
“Riuck01” <tforest@gaming.tech>
2023-02-21 17:15:38 +0100

    grille de jeu ok

--- main.c
+++ main.c
@@ -1,14 +1,40 @@
 #include <time.h>
 #include <stdio.h>
+char matrice[10][10];
+int row, columns;
 
-int main() {
+int main()
+{
 
-    int matrice[5][5] = {
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
+    // int matrice[10][10] = {
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    // };
+    
+    printf("  | 1  2  3  4  5  6  7  8  9  10\n");
+    printf("  --------------------------------\n");
+
+    for (row = 0; row < 10; row++)
+    {
+        if (row  < 9)
+            printf(" %d | ", row+1 );
+        else
+            printf("%d | ", row+1 );
+        
+        for (columns = 0; columns < 10; columns++)
+    {
+        printf("%c 0 ", matrice[row][columns] );
+    }
+        printf("\n");
+
     };
 
     srand(time(0));
@@ -16,9 +42,9 @@ int main() {
     printf("The random number is %d!\n\n", r);
 
     int row, columns;
-    for (row=0; row<5; row++)
+    for (row=0; row<10; row++)
     {
-        for(columns=0; columns<5; columns++)
+        for(columns=0; columns<10; columns++)
         {
             printf("-%d-", matrice[row][columns]);
         }
